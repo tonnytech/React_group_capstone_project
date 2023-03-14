298b9ef29e5bb940493edb7ae2a7d51e991c32fc
teitonny <tonnytei4@gmail.com>
2023-03-14 18:18:37 +0300

    resolve git conflict

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,42 @@
-/* eslint-disable */
 import { NavLink } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => (
-    
   <div>
     <ul>
-      <li>
-        <NavLink
-          to="/my-profile"
-          style={({ isActive }) => ({
-            color: isActive ? '#fff' : '#545e6f',
-            background: isActive ? '#7600dc' : '#f0f0f0',
-          })}
-        >
-          {' '}
-          My Profile
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          to="/missions"
-          style={({ isActive }) => ({
-            color: isActive ? '#fff' : '#545e6f',
-            background: isActive ? '#7600dc' : '#f0f0f0',
-          })}
-        >
-          {' '}
-          Missions
-        </NavLink>
-      </li>
+      <div className="logo">
+        <img src="https://cdn-icons-png.flaticon.com/512/3212/3212567.png" alt="" />
+        <h2>space travelers&apos; hub</h2>
+      </div>
+      <div className="links">
+        <li>
+          <NavLink
+            className="nav"
+            to="/Rockets"
+          >
+            {' '}
+            Rockets
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className="nav"
+            to="/my-profile"
+          >
+            {' '}
+            My Profile
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className="nav"
+            to="/missions"
+          >
+            {' '}
+            Missions
+          </NavLink>
+        </li>
+      </div>
     </ul>
   </div>
 );
