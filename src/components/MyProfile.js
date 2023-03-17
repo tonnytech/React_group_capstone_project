fa7b649e642ca425104cdd366d34cb83bdeb30f5
teitonny <tonnytei4@gmail.com>
2023-03-17 15:18:03 +0300

    profile setup

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,5 +1,15 @@
-const MyProfile = () => (
+/* eslint-disable */
+
+import { useSelector, useDispatch } from "react-redux";
+
+const MyProfile = () => {
+  const dispatch = useDispatch;
+  const { MissionData } = useSelector((state) => state.Missions);
+
+  return (
   <div>My profile</div>
 );
+  }
 
 export default MyProfile;
+
